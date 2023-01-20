6fcb22fca95264ec76e2b833ee2a14f2e7dd19f7
Martin Marmsoler <martin.marmsoler@gmail.com>
2023-01-20 18:24:47 +0100

    do not fetch library if already installed

--- script/mdflib.cmake
+++ script/mdflib.cmake
@@ -1,12 +1,16 @@
-include (FetchContent)
-FetchContent_Declare(mdflib
-        GIT_REPOSITORY https://github.com/ihedvall/mdflib.git
-        GIT_TAG HEAD)
-set(MDF_DOC OFF)
-set(MDF_TEST OFF)
-set(MDF_TOOLS OFF)
-FetchContent_MakeAvailable(mdflib)
+find_package(MdfLib)
+if (NOT MdfLib_FOUND)
+    include (FetchContent)
+    FetchContent_Declare(mdflib
+            GIT_REPOSITORY https://github.com/ihedvall/mdflib.git
+            GIT_TAG HEAD)
+    set(MDF_DOC OFF)
+    set(MDF_TEST OFF)
+    set(MDF_TOOLS OFF)
+    FetchContent_MakeAvailable(mdflib)
 
-message(STATUS "MDFLIB Populated: " ${mdflib_POPULATED})
-message(STATUS "MDFLIB Source Dir: " ${mdflib_SOURCE_DIR})
-message(STATUS "MDFLIB Binary Dir: " ${mdflib_BINARY_DIR})
+    message(STATUS "MDFLIB Populated: " ${mdflib_POPULATED})
+    message(STATUS "MDFLIB Source Dir: " ${mdflib_SOURCE_DIR})
+    message(STATUS "MDFLIB Binary Dir: " ${mdflib_BINARY_DIR})
+    set(MDFLIB_LOCAL_FETCH 1)
+endif()
