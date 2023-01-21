c25cbd3a12ff508739e3abb8e2ecc3a2a6089fbf
ihedvall <ihedvall@telia.com>
2023-01-21 15:15:49 +0100

    Try to fix flex build error

--- script/flex.cmake
+++ script/flex.cmake
@@ -3,6 +3,6 @@
 
 include(CMakePrintHelpers)
 if (NOT FLEX_FOUND)
-    find_package(FLEX)
+    find_package(FLEX 2.6)
     cmake_print_variables(FLEX_FOUND FLEX_EXECUTABLE FLEX_VERSION FLEX_LIBRARIES FLEX_INCLUDE_DIRS)
 endif()
